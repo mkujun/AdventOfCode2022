114c07e4f9d4cd36106008adf19e0667f0b0d771
mkujun <kujundzic.marko@hotmail.com>
2022-12-01 21:40:40 +0100

    day 1

--- src/01.ts
+++ src/01.ts
@@ -1,19 +1,38 @@
 import { readFileSync } from 'fs';
-const file: number[] = readFileSync('../inputs/01.txt', 'utf-8').split('\n').slice(0, -1).map(Number);
+const file: number[] = readFileSync('../inputs/01.txt', 'utf-8').split('\n').map(Number);
 
 let sum: number = 0;
-let max: number = 0;
+let topThree: number[] = [];
+
+function compareNumbers(a: number, b: number) {
+  return a - b;
+}
 
 file.forEach((e: number) => {
-  if(e > 0) {
+  if (e > 0) {
     sum = sum + e;
   }
-  else {
-    if (sum > max) {
-      max = sum;
+  else if (e === 0) {
+    if(topThree.length < 3) {
+      topThree.push(sum);
     }
+    else if (topThree.length === 3) {
+      topThree.sort(compareNumbers);
+      if (topThree[0] < sum) {
+        topThree[0] = sum;
+      }
+    }
+
     sum = 0;
   }
 })
 
-console.log("max", max);
+console.log("part 1", topThree[2]);
+
+const initialValue:number = 0;
+const sumWithInitial:number = topThree.reduce(
+  (accumulator:number, currentValue:number) => accumulator + currentValue,
+  initialValue
+);
+
+console.log("part 2", sumWithInitial);
