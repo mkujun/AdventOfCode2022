c25c01c7e42d53b77a5e29163cd019eed4e77ba3
mkujun <kujundzic.marko@hotmail.com>
2022-12-02 17:26:19 +0100

    day 2 part 2

--- src/02.ts
+++ src/02.ts
@@ -1,19 +1,8 @@
 import { readFileSync } from 'fs';
-//const input: string[] = readFileSync('../inputs/test.txt', 'utf-8').split('\n').slice(0, -1);
 const input: string[] = readFileSync('../inputs/02.txt', 'utf-8').split('\n').slice(0, -1);
 
-// first column (oponent)
-// A - Rock
-// B - Paper
-// C - Scissors
 
-// second column (you)
-// X - Rock
-// Y - Paper
-// Z - Scissors
-//
-
-function roundScore(oponent: string, you: string): number {
+function part1(oponent: string, you: string): number {
   switch(you) {
     case 'X':
     switch(oponent) {
@@ -49,15 +38,55 @@ function roundScore(oponent: string, you: string): number {
   return 0;
 }
 
-let sum: number = 0;
+function part2(oponent: string, you: string): number {
+  switch(oponent) {
+    case 'A': // Rock
+    switch(you) {
+        case 'X': // Scissors
+          return 3;
+        case 'Y': // Rock
+          return 4;
+        case 'Z': // Paper
+          return 8;
+      }
+    break;
+    case 'B': // Paper
+      switch(you) {
+        case 'X': // Rock
+          return 1;
+        case 'Y': // Paper
+          return 5;
+        case 'Z': // Scissors
+          return 9;
+      }
+    break;
+    case 'C': // Scissors
+      switch(you) {
+        case 'X': // Paper
+          return 2;
+        case 'Y': // Scissors
+          return 6;
+        case 'Z': // Rock
+          return 7;
+      }
+    break;
+  }
+  return 0;
+}
+
+let sum1: number = 0;
+let sum2: number = 0;
 
 input.forEach((e: string) => {
   let row = e.split('');
   let oponent: string = row[0];
   let you: string = row[2];
 
-  const score: number = roundScore(oponent, you);
-  sum = sum + score;
+  const score1: number = part1(oponent, you);
+  const score2: number = part2(oponent, you);
+  sum1 = sum1 + score1;
+  sum2 = sum2 + score2;
 })
 
-console.log("sum", sum);
+console.log("part 1", sum1);
+console.log("part 2", sum2);
